4a1dbba29d8eab6ab29cc0c997269b789c8bbfbe
Mikhail.Kargapolcev <KargapolcevME@sinara-group.com>
2025-05-24 08:09:51 +0300

    add: Добавлены файлы проекта.

---
+++ FW/src/main.h
@@ -0,0 +1,19 @@
+// K1921VK035 main.h
+// 18.04.2025
+// Михаил Каа
+
+ #ifndef MAIN_H
+ #define MAIN_H
+ 
+ #ifdef __cplusplus
+ extern "C" {
+ #endif
+ 
+
+
+ #ifdef __cplusplus
+ }
+ #endif
+ 
+ #endif // MAIN_H
+ 
